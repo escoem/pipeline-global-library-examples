7636e3359f3e4467835794cf7d17e6b1c895950b
Emilio Escobar <escoem@users.noreply.github.com>
2016-11-16 12:19:50 +0100

    class

--- src/escoem/CallableExample.groovy
+++ src/escoem/CallableExample.groovy
@@ -1,7 +1,10 @@
 package escoem;
 
-static class CallableExample extends jenkins.security.MasterToSlaveCallable<Void, Exception> implements Serializable {
+class CallableExample extends jenkins.security.MasterToSlaveCallable<Void, Exception> implements Serializable {
+    private static long serialVersionUID = -1L;
+    public CallableExample() {        
+    }
     public Void call() throws Exception {
-        System.out.println("HOLA SLAVE!!!")
+        System.out.println("HOLA SLAVE!!!");
     }
 }
