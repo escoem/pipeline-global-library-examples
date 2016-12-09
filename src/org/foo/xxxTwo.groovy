f6420a3e1a2cba687f70d55e36ba766547f915d1
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-09 15:56:55 +0100

    Create xxxTwo.groovy

---
+++ src/org/foo/xxxTwo.groovy
@@ -0,0 +1,23 @@
+
+
+def call(appenviron_properties) {
+
+  echo appenviron_properties.getModuleDir()
+  testCheckpoint ('first')
+
+  node ('master') {
+    echo 'inside master'
+    echo appenviron_properties.getModuleDir()
+    echo 'yes'
+  }
+
+}
+
+@NonCPS
+def testCheckpoint(state){
+    try {
+        checkpoint(state)
+    } catch (NoSuchMethodError _) {
+        echo 'Checkpoint feature available in CloudBees Jenkins Enterprise.'
+    }
+}
