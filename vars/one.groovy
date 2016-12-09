832293da9d0a63becf5aa731b8060f2f19f0fc46
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-09 16:00:26 +0100

    Update one.groovy

--- vars/one.groovy
+++ vars/one.groovy
@@ -6,7 +6,7 @@ def call(def buildnumber, def environment) {
     
     checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '037dbe61-9a5b-417e-a7fd-25ebf9249ab7', url: 'git@github.com:escoem/pipeline-global-library-examples.git']]]
     
-    groovy_app=load 'vars/properties.groovy'
+    groovy_app=load 'my_properties.groovy'
     echo groovy_app.moduleDir
   }
 
