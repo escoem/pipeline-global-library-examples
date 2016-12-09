273e508069f33e80a6475954e0d3fa8be59f3274
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-09 15:58:39 +0100

    Create one.groovy

---
+++ vars/one.groovy
@@ -0,0 +1,15 @@
+def groovy_app
+
+def call(def buildnumber, def environment) {
+
+  node ('slave'){
+    
+    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '037dbe61-9a5b-417e-a7fd-25ebf9249ab7', url: 'git@github.com:escoem/pipeline-global-library-examples.git']]]
+    
+    groovy_app=load 'vars/properties.groovy'
+    echo groovy_app.moduleDir
+  }
+
+  two(groovy_app)
+
+}
