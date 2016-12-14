f72b0c55615440eb4528f05488cb00344646b63f
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-14 11:21:15 +0100

    Update printClassPath.groovy

--- vars/printClassPath.groovy
+++ vars/printClassPath.groovy
@@ -1,11 +1,11 @@
 @NonCPS def call(classLoader) {
   System.out.print ("$classLoader")
   //((groovy.lang.GroovyClassLoader.InnerLoader)classLoader).getURLs().each {url->
-  classLoader.getURLs().each {url->
+  /*classLoader.getURLs().each {url->
      System.out.println ("- ${url.toString()}")
+  }*/
+  if (classLoader.parent) {
+     printClassPath(classLoader.parent)
   }
-  //if (classLoader.parent) {
-  //   printClassPath(classLoader.parent)
-  //}
 }
 //printClassPath this.class.classLoader
