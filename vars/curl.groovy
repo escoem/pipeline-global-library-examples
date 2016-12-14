cb27bacb5e3d35c9564858d2374e74c52c2033e2
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-14 12:09:59 +0100

    Update curl.groovy

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -4,7 +4,7 @@
   @Grab('org.apache.httpcomponents:httpclient:4.5.2')
 ])*/
 @Grab('org.apache.httpcomponents:httpclient:4.5.2')
-@GrabConfig(initContextClassLoader = false, systemClassLoader=true )
+@GrabConfig(initContextClassLoader = true, systemClassLoader=false )
 //@GrabConfig(systemClassLoader=true)
 
 
