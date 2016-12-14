3c119b29da366190abd2a9d6cdc97e462f758e0d
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-14 10:26:33 +0100

    Update curl.groovy

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -1,13 +1,14 @@
-@Grapes([
-@GrabResolver(name='httpclient', root='https://repo1.maven.org/maven2/', m2Compatible='true'),
-@GrabConfig(systemClassLoader=true),
-@Grab('org.apache.httpcomponents:httpclient:4.5.2')
-])
+@GrabResolver(name='httpclient', root='https://repo1.maven.org/maven2/', m2Compatible='true')
+//@GrabConfig(systemClassLoader=true)
+//@Grab('org.apache.httpcomponents:httpclient:4.5.2')
+
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.HttpClients;
+
+groovy.grape.Grape.grab(group:'org.apache.httpcomponents', module:'httpclient', version:'4.5.2')
 
 def call(String restURL){
 
