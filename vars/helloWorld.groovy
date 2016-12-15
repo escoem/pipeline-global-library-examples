6f76378492d5f2abb541972783febd97239eadd7
Emilio Escobar <escoem@users.noreply.github.com>
2016-12-15 17:53:36 +0100

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -2,5 +2,5 @@
 // vars/helloWorld.groovy
 def call(name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    echo "Hello world, ${name}"
+    echo "MASTER Hello world, ${name}"
 }
