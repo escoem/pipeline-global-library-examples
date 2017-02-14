e2bfe98b30d8c0f9e1968a1bdd2d1150f954a2a5
Emilio Escobar <escoem@users.noreply.github.com>
2017-02-14 10:49:25 +0100

    Create JobNameInfo.groovy

---
+++ org/foo/shared/JobNameInfo.groovy
@@ -0,0 +1,28 @@
+package org.foo.shared;
+//Class for the object for jobinfo.groovy
+public class JobNameInfo implements Serializable {
+  String remotePath = "";
+  String jobUrl = "";
+  public JobNameInfo(String remotePathParam, String jobURLParam){
+    this.remotePath = remotePathParam;
+    this.jobUrl = jobURLParam;
+  }
+
+  /**Get the remote path for trigger remote and copy artifact remote*/
+  public String getRemotePath(){
+    return remotePath;
+  }
+  /**Get the jenkins job url */
+  public String getJobURL(){
+    return jobURL;
+  }
+  /**GEt the short job name without folder full path */
+  public String getShortName(){
+    int index = remotePath.lastIndexOf("/");
+    if(index != -1){
+      return remotePath.substring(index);
+    }else{
+      return remotePath;
+    }
+  }
+}
