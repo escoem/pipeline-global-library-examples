fb60e4ef6e72e9df007b755c7de4b21a782939d6
Emilio Escobar <escoem@users.noreply.github.com>
2017-05-25 11:00:51 +0200

    Create PostGroovy2.groovy

---
+++ src/org/foo/shared/PostGroovy2.groovy
@@ -0,0 +1,25 @@
+package org.foo.shared
+
+import hudson.model.*
+import jenkins.model.*
+import java.util.concurrent.atomic.AtomicInteger
+import groovy.io.FileType
+
+
+class PostGroovy{
+
+    def steps
+
+    def PostGroovy(steps){
+        this.steps = steps
+    }
+	
+    def run(){
+	if (steps.manager.logContains("foo = bar")){
+                steps.manager.buildFailure()
+                steps.manager.addErrorBadge("found")
+                steps.manager.createSummary("warning.gif").appendText("<h1>found</h1>", false, false, false, "red")
+     
+        } 
+   }
+}
