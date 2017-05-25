e6e0c070795f2e1021637a23ee548e6954775bf2
Emilio Escobar <escoem@users.noreply.github.com>
2017-05-25 14:43:00 +0200

    Create SomeClass.groovy

---
+++ src/com/eer/SomeClass.groovy
@@ -0,0 +1,15 @@
+/**
+ * Created by  on 5/18/2017.
+ */
+
+package com.eer
+
+@GrabResolver(name='rnd-rpo', root='http://uk.maven.org/maven2/', m2Compatible = true )
+@Grab('org.yaml:snakeyaml:1.17')
+
+
+class SomeClass{
+
+    SomeClass(){}
+
+}
