c4a2d4f8823b31c960013b27443aa123c3526cbd
Emilio Escobar <escoem@users.noreply.github.com>
2017-10-05 11:25:57 +0200

    Create environmentVars.groovy

---
+++ vars/environmentVars.groovy
@@ -0,0 +1,8 @@
+// vars/environmentVars.groovy
+def artifactoryUrl = 'https://artifactory.com/artifactory'
+def artifactoryServerId = '-123@123'
+def artifactoryCredId = 'ArtifactoryID'
+def npmRepositorySuffix = 'api/npm/npm-virtual'
+def approver = 'esurance1'
+def promoters = 'esurance1@esurance.com'
+def BITBUCKET_URL = 'https://esurance.bitbucket.com/scm'
