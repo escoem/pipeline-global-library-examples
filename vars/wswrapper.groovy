544d4864a1fd936ab5a878e15383f75240505932
Emilio Escobar <escoem@users.noreply.github.com>
2017-10-11 10:58:29 +0200

    Update wswrapper.groovy

--- vars/wswrapper.groovy
+++ vars/wswrapper.groovy
@@ -19,8 +19,19 @@ def call() {
       myMessage= myMessage+"</soap-env:Body>\\n"
       myMessage= myMessage+"</soap-env:Envelope>\\n"
 */
-  def myMessage="<?xml version='1.0' encoding='UTF-8'?><soap-env:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><soap-env:Body><GetWeather xmlns='http://www.webserviceX.NET'><CityName>Paris</CityName><CountryName>France</CountryName></soap-env:Body></soap-env:Envelope>"
+  //def myMessage="<?xml version='1.0' encoding='UTF-8'?><soap-env:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><soap-env:Body><GetWeather xmlns='http://www.webserviceX.NET'><CityName>Paris</CityName><CountryName>France</CountryName></soap-env:Body></soap-env:Envelope>"
 		
+  def myMessage='''	
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <GetWeather xmlns="http://www.webserviceX.NET">
+      <CityName>Paris</CityName>
+      <CountryName>France</CountryName>
+    </GetWeather>
+  </soap:Body>
+</soap:Envelope>
+'''		
 		
   def client = new SOAPClient('http://www.webservicex.com/globalweather.asmx')
 	def response = client.send(myMessage)
