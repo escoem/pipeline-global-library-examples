7d7ee583f54834e0b192c6ae7ce9e348d63aeea4
Emilio Escobar <escoem@users.noreply.github.com>
2017-10-11 10:40:48 +0200

    Create wswrapper.groovy

---
+++ vars/wswrapper.groovy
@@ -0,0 +1,29 @@
+// vars/wswrapper.groovy
+@Grab('com.github.groovy-wslite:groovy-wslite:1.1.2') 
+
+import wslite.http.auth.*; 
+import wslite.rest.*; 
+import wslite.soap.*; 
+import wslite.*; 
+import jenkins.util.*; 
+import jenkins.model.*;
+
+@NonCPS
+def soapcall() {
+  
+  def myMessage="<?xml version='1.0' encoding='UTF-8'?>"+"\n"
+	    myMessage= myMessage+"<soap-env:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'>"+"\n"
+	    myMessage= myMessage+"<soap-env:Body>"+"\n"
+      myMessage= myMessage+"<GetWeather xmlns='http://www.webserviceX.NET'>"+"\n"
+	    myMessage= myMessage+"<CityName>Paris</CityName>"+"\n"
+	    myMessage= myMessage+"<CountryName>France</CountryName>"+"\n"
+      myMessage= myMessage+"</soap-env:Body>"+"\n"
+      myMessage= myMessage+"</soap-env:Envelope>"+"\n"
+       
+  def client = new SOAPClient('http://www.webservicex.com/globalweather.asmx')
+	def response = client.send(message)
+     
+	return response.text
+}
+
+return this
