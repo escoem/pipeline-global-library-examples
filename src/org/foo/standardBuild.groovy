3c2adc54478ee11d76e4102b3e3ae7001609beed
Emilio Escobar <escoem@users.noreply.github.com>
2018-02-05 19:43:06 +0100

    Update standardBuild.groovy

--- src/org/foo/standardBuild.groovy
+++ src/org/foo/standardBuild.groovy
@@ -1,5 +1,28 @@
 package org.foo
 
+def api; 
+def lib;
+
+def dotnetapi(cfg)
+{
+    def build = new dotnetApiBuild()
+    cfg.resolveStrategy = Closure.DELEGATE_FIRST
+    cfg.delegate = build
+    cfg()
+    //api = build
+    standardBuildSpec.builds[build.projectName] = build
+}
+
+def dotnetlib(cfg)
+{
+    def build = new dotnetlibBuild()
+    cfg.resolveStrategy = Closure.DELEGATE_FIRST
+    cfg.delegate = build
+    cfg()
+    lib = build
+    standardBuildSpec.builds[build.projectName] = build
+}
+
 def execute()
 {
     node ()
