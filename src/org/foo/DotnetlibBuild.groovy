8e42ce626af6e9dd2cda3712f5af10c9182e405a
Emilio Escobar <escoem@users.noreply.github.com>
2018-02-06 19:40:09 +0100

    Update and rename dotnetlibBuild.groovy to DotnetlibBuild.groovy

---
+++ src/org/foo/DotnetlibBuild.groovy
@@ -0,0 +1,25 @@
+package org.foo
+
+class DotnetlibBuild {
+    
+    String projectName
+    String projectDir
+    def steps
+    
+    DotnetlibBuild(steps) {
+        this.steps = steps
+    }
+    
+def execute()
+{
+    steps.echo "Executing dotnetlibBuild. Project name: ${projectName}; Project dir: ${projectDir}"
+
+    /*
+    dotnet.restore path, jfrog.url_all_nuget
+    dotnet.publish config.projectName, config.target
+    rpm.build(config.projectName)
+    jfrog.publish(config.projectName)
+    */
+}
+
+}
