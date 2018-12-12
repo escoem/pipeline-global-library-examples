7967a8f08bd78df072dc682390513eb5de3dfece
Emilio Escobar <escoem@users.noreply.github.com>
2018-12-12 11:37:01 +0100

    Update buildBy.groovy

--- vars/buildBy.groovy
+++ vars/buildBy.groovy
@@ -1,7 +1,5 @@
 //vars/buildBy.groovy
 def call () {
-  echo currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).get(0).
-        findCause(hudson.model.Cause.UserIdCause.class).getUserId()
   return currentBuild.rawBuild.getActions(hudson.model.CauseAction.class).get(0).
         findCause(hudson.model.Cause.UserIdCause.class).getUserId()
 }
